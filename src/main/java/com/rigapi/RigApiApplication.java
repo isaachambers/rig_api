efdf021f0d2866c7c6bdfd6a970ca9feff04193a
Isaac Kiyimba <isaaceduardk@gmail.com>
2020-11-02 08:20:17 +0300

    Project setup

---
+++ src/main/java/com/rigapi/RigApiApplication.java
@@ -0,0 +1,13 @@
+package com.rigapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RigApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RigApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/rigapi/RigApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rigapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RigApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
